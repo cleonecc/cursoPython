5edb41e6e451d552747ff78b56b88cdc882d589a
cleonecc <38408939+cleonecc@users.noreply.github.com>
2018-04-21 22:32:24 -0300

    Update ex10.py

--- ex10.py
+++ ex10.py
@@ -1,7 +1,7 @@
 # programa que solicita uma temperatura em graus Celsius e converte para graus Farenheit
 
 # solicita a temperatura em graus Celsius
-temperatura_celsius = float(input("Informe a temperatura em graus Celsius: ")
+temperatura_celsius = float(input("Informe a temperatura em graus Celsius: "))
 # Converte a temperatura de Celsius para Farenheit
 temperatura_farenheit = temperatura_celsius * 1.8 + 32
 # apresenta o resultado da conversão
