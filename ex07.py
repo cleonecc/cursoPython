1310af64bdb71286a70b0e019856c80e96fbe12e
cleonecc <38408939+cleonecc@users.noreply.github.com>
2018-04-21 22:51:31 -0300

    Update ex07.py

--- ex07.py
+++ ex07.py
@@ -1,6 +1,10 @@
 # programa que calcula a área de um quadrado e mostra o dobro da área calculada
 
+# solicita o valor referente ao lado do quadrado
 lado_quadrado = int(input("Informe o tamanho do lado do quadrado: "))
+# calcula a área do quadrado
 area_quadrado = lado_quadrado ** 2
+# multiplica o valor da área do quadrado por 2
 tamanho_final = area_quadrado * 2
+# apresenta o resultado final
 print("O dobro da área calculada é ", tamanho_final)
