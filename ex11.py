1ede085dda9c2fc85fa5ed38d712149302077993
cleonecc <38408939+cleonecc@users.noreply.github.com>
2018-04-21 22:53:31 -0300

    Update ex11.py

--- ex11.py
+++ ex11.py
@@ -1,9 +1,13 @@
 # programa que solicita dois números inteiros e um número real e faz cálculos conforme o enunciado.
 
+# importa a biblioteca matemática
 import math
 
+# solicita um número inteiro
 numero01 = int(input("Informe um número inteiro: "))
+# solicita outro número inteiro
 numero02 = int(input("Informe outro número inteiro: "))
+# solicita um número real
 numero03 = float(input("Informe um número real: "))
 
 print("O produto do dobro do primeiro com metade do segundo é ",((numero01*2)*(numero02/2)))
